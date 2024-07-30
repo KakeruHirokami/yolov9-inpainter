a08d29a013693932993feb3d2ffa617286e4f9e8
KakeruHirokami <kakeru.hirokami.stonerider@gmail.com>
2024-07-30 22:14:39 +0900

    Add smple gif

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 WEIGHTS_PATH="./runs/train/exp5/weights/best.pt"
-VIDEO_PATH="20240630_085506000_iOS.MOV"
+VIDEO_PATH="sample.mp4"
 OUTPUT_PATH="output.mp4"
 
 subprocess.call([
